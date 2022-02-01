834f545ec717b4b1e744379d8667d1d233f60062
Héctor Plinio Navarro Navarro <73659203+hectorplinio@users.noreply.github.com>
2022-02-01 20:45:15 +0100

    Ya

---
+++ app.js
@@ -0,0 +1,39 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+async function fetchProducts() {
+    const response = await fetch(
+      "https://vercel-app-futbol.herokuapp.com/players",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data);
+        const ul = document.getElementById('players');
+        let players = data;
+        //console.log(products);
+        
+        for(let player of players){
+            let li = createNode('li');
+            let span = createNode('span');           
+            span.innerHTML = `${player.name} ${player.age} ${player.position}`;            
+            append(li, span);
+            append(ul, li);
+        }
+        
+      })
+      .catch((error) => console.log(error));
+  }
+
+  fetchProducts();
