22429e9d0356be93d6379c91a2be55d8fa1912b5
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2022-02-04 18:50:29 +0100

    a

--- app.js
+++ app.js
@@ -39,14 +39,14 @@ async function fetchProducts() {
             }else if(player.team == "BORUSSIA DORTMUND") {
               team = "<p><b>Team:</b>"+ player.team+ "</p><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Borussia_Dortmund_logo.svg/640px-Borussia_Dortmund_logo.svg.png' class='escudo'>";
             }
-            span.innerHTML += "<a href='"+player._id+"'>";
+            li.innerHTML += "<a href='"+player._id+"'>";
             span.innerHTML += "<div class='player'>";
             span.innerHTML += "<p><b>Name:</b>"+ player.name +"</p>";
             span.innerHTML += "<p><b>Age:</b>"+ player.age +"</p>";
             span.innerHTML += "<p><b>POS:</b>"+ player.position +"</p>";
             span.innerHTML += "<p><b>POS:</b>"+ player.position +"</p>";
             span.innerHTML += team;
-            span.innerHTML += "</a>";
+            li.innerHTML += "</a>";
 
 
             append(li, span);
