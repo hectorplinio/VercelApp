7b6b2137f104d4c6de6afd0efb6236bc96e92021
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2022-02-04 17:50:24 +0100

    a

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ async function fetchProducts() {
             let span = createNode('span');           
             span.innerHTML += `${player.name} ${player.age} ${player.position}`;   
             span.innerHTML += "<a href='/players/<%= player.id'>"; 
-            span.innerHTML += `<p><b>Name:</b> ${player.name} </p>`; 
+            span.innerHTML += `<p><b>Name:</b> `+player.name`+ </p>`; 
+            span.innerHTML += `<p><b>Age:</b> `+player.age`+ </p>`; 
+            span.innerHTML += `<p><b>POS:</b> `+player.position`+ </p>`; 
 
             append(li, span);
             append(ul, li);
