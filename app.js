737253a3b7403b5372d979454d4505c411ae498d
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2022-02-04 18:20:09 +0100

    a

--- app.js
+++ app.js
@@ -22,15 +22,14 @@ async function fetchProducts() {
         //console.log(data);
         const ul = document.getElementById('players');
         let players = data;
+        console.log(players);
         
         for(let player of players){
-            console.log(player)
             let li = createNode('li');
             let span = createNode('span');           
-            span.innerHTML += `<a href='/players/`+player['_id']`+'>`; 
-            span.innerHTML += `<p><b>Name:</b> `+player['name']`+ </p>`; 
-            span.innerHTML += `<p><b>Age:</b> `+player['age']`+ </p>`; 
-            span.innerHTML += `<p><b>POS:</b> `+player['position']`+ </p>`; 
+            span.innerHTML += `${player.name} ${player.age} ${player.position}`;   
+            span.innerHTML += "<a href='/players/<%= player.id'>"; 
+            span.innerHTML += `<p><b>Name:</b> `+player.name`+ </p>`; 
 
             append(li, span);
             append(ul, li);
