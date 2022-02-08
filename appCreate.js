096646dd750441499fb9db54dc6706c8a0911d95
Hector Plinio <hectorplinio@MacBook-Air-de-Hector.local>
2022-02-08 17:08:21 +0100

    create

--- appCreate.js
+++ appCreate.js
@@ -1,5 +1,5 @@
 const btnNew = document.getElementById("btnNew");
-btnNew.addEventListener("click",fetchCreateProduct);
+btnNew.addEventListener("click", fetchCreateProduct);
 
 function getPositions() {
   let positions = ["GK", "DF", "MF", "FW", "CH"];
@@ -17,43 +17,51 @@ function getPositions() {
 
   for (let i = 0; i < positions.length; i++) {
     optionPos +=
-      "<option value='"+positions[i]+"' id='position'>" +positions[i] +"</option>";
-
+      "<option value='" +
+      positions[i] +
+      "' id='position'>" +
+      positions[i] +
+      "</option>";
   }
-  for (let x = 0; x< teams.length; x++) {
+  for (let x = 0; x < teams.length; x++) {
     optionTeam +=
-      "<option value='"+teams[x]+"' id='team'>" +teams[x] +"</option>";
-
+      "<option value='" + teams[x] + "' id='team'>" + teams[x] + "</option>";
   }
   document.getElementById("position").innerHTML = optionPos;
   document.getElementById("team").innerHTML = optionTeam;
 }
 async function fetchCreateProduct() {
-	const name = document.getElementById("name").value;
+  const name = document.getElementById("name").value;
   const surname = document.getElementById("surname").value;
-	const age = document.getElementById("age").value;
-	const position = document.getElementById("position").value;
-	const team = document.getElementById("team").value;
-	console.log(name + " " + surname + " " + age+ " "+ position + " " + team);
-	
-	const newPlayer = {name: name, surname: surname, age:age, position:position, team:team};
-	
-    const response = await fetch(
-      "https://vercel-app-futbol.herokuapp.com/players",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-		body: JSON.stringify(newPlayer),
-    mode:"no-cors"
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        debugger;
-        console.log(data);
-      })
-      .catch((error) => console.log(error));
-  }
+  const age = document.getElementById("age").value;
+  const position = document.getElementById("position").value;
+  const team = document.getElementById("team").value;
+  console.log(name + " " + surname + " " + age + " " + position + " " + team);
+
+  const newPlayer = {
+    name: name,
+    surname: surname,
+    age: age,
+    position: position,
+    team: team,
+  };
+  debugger;
+
+  const response = await fetch(
+    "https://vercel-app-futbol.herokuapp.com/players",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlayer),
+      mode: "no-cors",
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+}
